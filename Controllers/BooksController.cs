897dc12bc85cd1a8c9cefd4fb3ea4e983eb0a738
Mohamed Abbas <Mohamed.abbas.cic@gmail.com>
2020-07-18 17:51:33 +0200

    BookListCore Commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookListMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null) // Create
+                return View( Book);
+
+            // Update
+            Book = await _context.Books.FirstOrDefaultAsync(b => b.Id == id);
+
+            if (Book == null)
+                return NotFound();
+
+
+            return View(Book);
+        }
+
+        [HttpPost]
+        public  IActionResult Upsert(Book Book)
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    // Create
+                    _context.Books.Add(Book);
+                }
+                else
+                {
+                    _context.Books.Update(Book);
+                }
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Book);
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new {data = await _context.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDB = await _context.Books.FirstOrDefaultAsync(b => b.Id == id);
+            if (bookFromDB == null)
+                return Json(new { success = false, message = "Cannot Find This book,please Reload the page and make sure its already exisits." });
+
+            _context.Books.Remove(bookFromDB);
+            _context.SaveChanges();
+            return Json(new { success = true, message = "Delete Successfully."});
+        }
+        #endregion
+    }
+}
---
+++ wwwroot/js/BookList.js
@@ -0,0 +1,73 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "../Books/GetAll/",
+            "type": "GET",
+            "dataType": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return '<div class="text-center">' +
+                        '<a href="../Books/Upsert?id=' + data + '" class="btn btn-success text-white" style="cursor:pointer;width:30%">Edit</a> &nbsp;' +
+                        '<a class="btn btn-danger text-white" style="cursor:pointer;width:30%" onclick=Delete("../Books/Delete?id=' + data + '")>Delete</a>' +
+                        '</div>';
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No Books Found."
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure you want to delete this book?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText :'No, cancel the order'
+    }).then((willDelete) => {
+        if (willDelete.value) {
+
+            $.ajax({
+                type: "Delete",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                        Swal.fire(
+                            'Deleted!',
+                            data.message,
+                            'success'
+                        )
+                    }
+                    else {
+                        toastr.error(data.message);
+                        Swal.fire(
+                            'Error!',
+                            data.message,
+                            'error'
+                        )
+                    }
+                }
+            })
+
+        }
+    })
+}
